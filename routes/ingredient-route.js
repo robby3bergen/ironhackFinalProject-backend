3006019f477fd79b7b56af9b27dbfe66246515d3
Robby van Rheenen <robbyvanrheenen@gmail.com>
2018-12-19 22:17:45 +0100

    populate user ingredients preferences with ingredient name

--- routes/ingredient-route.js
+++ routes/ingredient-route.js
@@ -6,9 +6,24 @@ const User = require('../models/user');
 
 const { isLoggedIn } = require('../helpers/middlewares');
 
-router.get('/list', (req, res, next) => {
+// ========= Router: gets the user preferences for the ingredients
+router.post('/list', (req, res, next) => {
   if (req.session.currentUser) {
-    res.json([{ 'ingredient': 'salt' }, { 'ingredient': 'pepper' }]);
+    // find any ingredient inside the array of ingredient objects
+    const { ingredients } = req.body;
+    Ingredient.find({ name: ingredients })
+      .then((ingredientsInDB) => {
+        console.log('ingredientsInDB: ' + ingredientsInDB);
+        // find user preference for ingredient
+        User.findOne({ _id: req.session.currentUser._id })
+          .populate('ingredients.ingredient_id')
+          .then((user) => {
+            console.log('user: ' + user);
+            res.json(user);
+          });
+        // res.json(user[0].ingredients);
+      })
+      .catch(next);
   } else {
     res.status(404).json({
       error: 'unauthorized'
@@ -16,6 +31,7 @@ router.get('/list', (req, res, next) => {
   }
 });
 
+// ========== Router: saves ingredients and user preferences
 router.post('/', (req, res, next) => {
   // check if user is logged in
   if (!req.session.currentUser) {
@@ -24,10 +40,8 @@ router.post('/', (req, res, next) => {
     });
   }
 
-  console.log(req.body);
-
   // create or update ingredient
-  const ingredientName = req.body[0];
+  const { ingredientName, userPreference } = req.body;
   Ingredient.findOneAndUpdate(
     { name: ingredientName },
     { $set: { name: ingredientName } },
@@ -35,14 +49,13 @@ router.post('/', (req, res, next) => {
   )
     .then((ingredient) => {
       // create or update user preference
-      const newUserIngredient = { ingredient_id: ingredient._id, preference: 'avoid' };
+      const newUserIngredient = { ingredient_id: ingredient._id, preference: userPreference };
       User.findOneAndUpdate(
         { _id: req.session.currentUser._id, 'ingredients.ingredient_id': ingredient._id },
         { $set: { 'ingredients.$.preference': 'favorite' } }, /* $ stores the id of the found object to make it change only that one */
         { new: true }
       )
         .then((user) => {
-          console.log(user);
           if (!user) {
             User.findById(req.session.currentUser._id)
               .then((user) => {
@@ -51,6 +64,7 @@ router.post('/', (req, res, next) => {
               });
           }
         });
+      res.json('ingredients and user preferences saved');
     })
     .catch(next);
 });
